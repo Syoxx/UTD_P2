bed0b5a22e0ddbd49fa355f27181a4df5c821f79
npietrek <npietrek@e1837b7c-87a1-41f4-63f6-ce57b7db7bff>
2018-09-20 13:25:03 +0000

    added first base classes
    git-svn-id: https://github.com/Syoxx/UTD_P2.git/trunk@2 e1837b7c-87a1-41f4-63f6-ce57b7db7bff

---
+++ UTD_P2/UTD_P2/Enemys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTD_P2
+{
+	class Enemys
+	{
+		private float moveSpeed;
+		private float life;
+	}
+}
